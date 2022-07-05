2a0fcef7f91bbf09f1dff4fc8b8a337c044bb80b
Evgrashin.Dmitriy <Evgrashin.Dmitriy@yandex.ru>
2022-07-05 20:43:39 +0400

    made a slider video

--- src/script/script.js
+++ src/script/script.js
@@ -1,11 +1,14 @@
 const hamburger = document.querySelector(".b-header-main__hamburger"),
       navContainer = document.querySelector(".b-hamburger-main");
 
-    hamburger.onclick = function() {
+      $(hamburger).on('click', function() {
         hamburger.classList.toggle("active")
         navContainer.classList.toggle("active")
-    }
+    })
 
 $(document).ready(function() {
-    $('.b-slider-video__picture').slick();
+    $('.b-slider-video').slick({
+        arrows: true,
+        dots: true,
+    });
 })
